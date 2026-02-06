92d70708735da3e7ea7c462976dcd4b0ec84869d
John Francis <johnmosima7@gmail.com>
2026-02-06 16:13:01 +0300

    Fixed the Draggable bug

--- src/hoc/WindowWrapper.jsx
+++ src/hoc/WindowWrapper.jsx
@@ -1,7 +1,7 @@
 import useWindowStore from "#store/window"
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { Draggable } from "gsap/draggable";
+import { Draggable } from "gsap/Draggable";
 import { useLayoutEffect, useRef } from "react";
 
 const WindowWrapper = (Component, windowKey) => {
